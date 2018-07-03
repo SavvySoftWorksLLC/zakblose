63845baa5f72c1ba1b9956946578d53518ebbf80
Gary A. Newsome <garyanewsome@gmail.com>
2018-07-03 17:01:01 -0400

    EOD push, jumbo, calendar poc, gallery poc, load screen, icons

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,43 @@
-console.log('dr.')
 $(function() {
-  console.log('ass')
-  $(".filter-button").click(function(){
-      var value = $(this).attr('data-filter')
-      
-      if(value == "all") {
-          $('.filter').show('1000')
-      } else {
-        $(".filter").not('.'+value).hide('3000')
-        $('.filter').filter('.'+value).show('3000')
-      }
+  // jQuery Scroll Animations
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'))
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 500)
+    }
   })
-  
-  if ($(".filter-button").removeClass("active")) {
-    $(this).removeClass("active");
+
+  // jumbotron icons
+  // on load
+  $(".icons-calendar").show()
+  $(".icons-return").hide()
+  $(".icons-gallery").show()
+
+  // on scroll
+  $(document).scroll(function () {
+    if ($(window).scrollTop() >= 24) {
+      $(".icons-calendar").fadeOut(1000)
+      $(".icons-return").fadeIn(2000)
+      $(".icons-gallery").fadeOut(1000)
+    } else {
+      $(".icons-calendar").fadeIn(2000)
+      $(".icons-return").fadeOut(1000)
+      $(".icons-gallery").fadeIn(2000)
+    }
+  })
+
+  // splash overlay
+  $.fn.center = function () {
+    this.css("position","absolute")
+    return this
   }
-  $(this).addClass("active");
+  $("#overlay").show()
+  $("#overlay-content").show().center()
+  setTimeout(function(){    
+    $("#overlay").fadeOut( 1600, "linear" )
+  }, 500)
 })
-console.log('hat')
+
