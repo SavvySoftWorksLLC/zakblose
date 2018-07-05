e7c5f0d4ad4b20b183c00c2626ad799e58c4c2dd
Gary A. Newsome <garyanewsome@gmail.com>
2018-07-05 14:56:57 -0400

    Add README and build

---
+++ README.md
@@ -0,0 +1,17 @@
+# Zak Blose
+
+This site is built using Hugo [https://gohugo.io/](https://gohugo.io/)
+
+To run the site locally: `hugo server` and visit `http://localhost:1313`
+
+To Build the site: `hugo`
+
+
+## Needs
+
+- [ ] Images
+- [ ] Words
+- [ ] Social Media accounts?
+- [ ] About page?
+- [ ] Calendar creds
+
---
+++ docs/js/index.js
@@ -0,0 +1,55 @@
+$(function() {
+  // jQuery Scroll Animations
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'))
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 500)
+    }
+  })
+
+  // jumbotron icons
+  // on load
+  $(".icons-calendar").show()
+  $(".icons-return").hide()
+  $(".icons-gallery").show()
+
+  // on scroll
+  $(document).scroll(function () {
+    if ($(window).scrollTop() >= 24) {
+      $(".icons-calendar").fadeOut(1000)
+      $(".icons-return").fadeIn(2000)
+      $(".icons-gallery").fadeOut(1000)
+    } else {
+      $(".icons-calendar").fadeIn(2000)
+      $(".icons-return").fadeOut(1000)
+      $(".icons-gallery").fadeIn(2000)
+    }
+  })
+
+  // splash overlay
+  $.fn.center = function () {
+    this.css("position","absolute")
+    return this
+  }
+  $("#overlay").show()
+  $("#overlay-content").show().center()
+  setTimeout(function(){    
+    $("#overlay").fadeOut( 1600, "linear" )
+  }, 500)
+
+  // gallery
+  $(".filter-button").click(function(){
+    var value = $(this).attr('data-filter');
+
+    if(value == "all") {
+      $('.filter').show('1000');
+    } else {
+      $(".filter").not('.'+value).hide('2000');
+      $('.filter').filter('.'+value).show('2000')
+    }
+  })
+})
+
